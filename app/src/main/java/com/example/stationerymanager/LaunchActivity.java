22ecde9be595f82a7968d7a910e8cfef29167344
Nisuga Jayawardana <44756803+NisugaJ@users.noreply.github.com>
2019-08-04 13:15:14 +0530

    Added TimePicker, Expenses ListView part. Disabled Landscape mode

--- app/src/main/java/com/example/stationerymanager/LaunchActivity.java
+++ app/src/main/java/com/example/stationerymanager/LaunchActivity.java
@@ -41,7 +41,7 @@ public class LaunchActivity extends AppCompatActivity {
                     });
                     try {
                         // Sleep for 200 milliseconds.
-                        Thread.sleep(40);
+                        Thread.sleep(10);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
--- app/src/main/java/com/example/stationerymanager/MainNavigation.java
+++ app/src/main/java/com/example/stationerymanager/MainNavigation.java
@@ -33,6 +33,8 @@ public class MainNavigation extends AppCompatActivity {
                     break;
                 case R.id.navigation_stationery:
                     title = getString(R.string.title_stationery);
+                    Intent intent = new Intent(getApplicationContext(), AddSales.class);
+                    startActivity(intent);
                     break;
                 case R.id.navigation_printing:
                     title = getString(R.string.title_printing);
