f550c587210edd00bfb8ac55d66a4d73004be3e0
nuwanchiran <nuwanchiran@gmail.com>
2019-08-04 15:58:21 +0530

    fourth commit , fax and calls

--- app/src/main/java/com/example/stationerymanager/faxCalls.java
+++ app/src/main/java/com/example/stationerymanager/faxCalls.java
@@ -1,6 +1,7 @@
 package com.example.stationerymanager;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 
@@ -9,6 +10,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
@@ -20,11 +22,29 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class faxCalls extends Fragment {
+    View view;
+    Button call;
+    Button fax;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fax_calls, container, false);
+        View v = inflater.inflate(R.layout.fragment_fax_calls, container, false);
+        call = v.findViewById(R.id.addCalls);
+        fax = v.findViewById(R.id.addFax);
+
+        call.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(view.getContext(),Calls.class);
+                startActivity(intent);
+            }
+        });
+        fax.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(view.getContext(),Fax.class);
+                startActivity(intent);
+            }
+        });
+        return v;
     }
 }
