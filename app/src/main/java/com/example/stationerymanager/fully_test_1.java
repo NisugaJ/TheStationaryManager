2c1c32b77a72272f6ffda557e15035c7a51b8b4f
HansikaPerera <madhuhanz@gmail.com>
2019-08-05 15:35:52 +0530

    fully test 1 file added

---
+++ app/src/main/java/com/example/stationerymanager/fully_test_1.java
@@ -0,0 +1,14 @@
+package com.example.stationerymanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class fully_test_1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fully_test_1);
+    }
+}
